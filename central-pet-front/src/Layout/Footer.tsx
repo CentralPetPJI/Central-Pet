6b6160cfdd5ce1265ab2d87bc354a32d9d69cf56
edvardsanta <eduardosantamarco123@outlook.com>
2026-02-23 17:50:15 -0300

    refactor: melhorar componente Footer com layout fixo e responsivo

--- central-pet-front/src/Layout/Footer.tsx
+++ central-pet-front/src/Layout/Footer.tsx
@@ -1,11 +1,9 @@
-import "./Footer.css";
-
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="footer">
-      <div>
-        <p>Central Pet - 2024</p>
-      </div>
+    <footer className="fixed left-0 bottom-0 w-full h-[7vh] border-t border-gray-300 bg-linear-to-r from-white to-cyan-200 bg-opacity-80 text-center flex justify-center items-center z-50">
+      <p className="text-gray-900 opacity-80">Central Pet - {currentYear}</p>
     </footer>
   );
 };
