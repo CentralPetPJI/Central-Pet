de091136cacbb111e04a64c4a5f188d44407ec53
edvardsanta <eduardosantamarco123@outlook.com>
2026-02-23 17:51:38 -0300

    refactor: atualizar informações dos pets e corrigir estrutura de dados

--- central-pet-front/src/Models/Pet.tsx
+++ central-pet-front/src/Models/Pet.tsx
@@ -1,53 +1,65 @@
-import { Pet } from "./Types";
+import { Pet } from './Types';
 
 const petsData: Pet[] = [
   {
     id: 1,
-    name: "Buddy",
-    photo: "/alvan-nee-eoqnr8ikwFE-unsplash.jpg",
-    caracteristicasFisicas: "Golden retriever, 3 years old, male",
-    caracteristicasComportamentais: "Friendly, active, loves playing fetch",
-    observacoes: "Needs regular exercise and socialization",
+    name: 'Buddy',
+    species: 'dog',
+    photo:
+      'https://media.istockphoto.com/id/511375254/pt/foto/c%C3%A3o-com-um-grande-sorriso.jpg?s=1024x1024&w=is&k=20&c=JHfniereojm3lJsJN7JYOW-itWaY0csfJFYTVDDNsbI=',
+    physicalCharacteristics: 'Golden retriever, 3 years old, male',
+    behavioralCharacteristics: 'Friendly, active, loves playing fetch',
+    notes: 'Needs regular exercise and socialization',
+  },
+  {
+    id: 1,
+    name: 'Chuck',
+    species: 'dog',
+    photo:
+      'https://media.istockphoto.com/id/518963294/pt/foto/golden-retriever-c%C3%A3o.jpg?s=2048x2048&w=is&k=20&c=xu2p8ucqmPLzUfYjyV6ZE_fTR7fktX2qgwQtkhWncNw=',
+    physicalCharacteristics: 'Golden retriever, 3 years old, male',
+    behavioralCharacteristics: 'Friendly, active, loves playing fetch',
+    notes: 'Needs regular exercise and socialization',
   },
   {
     id: 2,
-    name: "Chuck",
-    photo: "/alvan-nee-ZCHj_2lJP00-unsplash.jpg",
-    caracteristicasFisicas: "Golden retriever, 3 years old, male",
-    caracteristicasComportamentais: "Friendly, active, loves playing fetch",
-    observacoes: "Needs regular exercise and socialization",
+    name: 'Portos',
+    species: 'cat',
+    photo:
+      'https://media.istockphoto.com/id/992637094/pt/foto/british-short-hair-cat-and-golden-retriever.jpg?s=1024x1024&w=is&k=20&c=8HKadn_i8OmYnkz2HhUx5EQYZPkazAJMrLQQtlK2OjU=',
+    physicalCharacteristics: 'British shorthair, 2 years old, female',
+    behavioralCharacteristics: 'Calm, affectionate, likes quiet places',
+    notes: 'Prefers indoor environments',
   },
   {
     id: 3,
-    name: "Portos",
-    photo: "/andrew-s-ouo1hbizWwo-unsplash.jpg",
-    caracteristicasFisicas: "Golden retriever, 3 years old, male",
-    caracteristicasComportamentais: "Friendly, active, loves playing fetch",
-    observacoes: "Needs regular exercise and socialization",
+    name: 'Lady',
+    species: 'cat',
+    photo:
+      'https://media.istockphoto.com/id/992637094/pt/foto/british-short-hair-cat-and-golden-retriever.jpg?s=1024x1024&w=is&k=20&c=8HKadn_i8OmYnkz2HhUx5EQYZPkazAJMrLQQtlK2OjU=',
+    physicalCharacteristics: 'British shorthair, 2 years old, female',
+    behavioralCharacteristics: 'Calm, affectionate, likes quiet places',
+    notes: 'Prefers indoor environments',
   },
   {
     id: 4,
-    name: "Lady",
-    photo: "/istockphoto-1503385646-170667a.webp",
-    caracteristicasFisicas: "Golden retriever, 3 years old, male",
-    caracteristicasComportamentais: "Friendly, active, loves playing fetch",
-    observacoes: "Needs regular exercise and socialization",
+    name: 'Pringles',
+    species: 'other',
+    photo:
+      'https://media.istockphoto.com/id/1325997570/pt/foto/bengal-cat-lying-on-sofa-and-smiling.jpg?s=1024x1024&w=is&k=20&c=tWRFOQpOAJXYWk5UzGjY0IXE0vslvP8yUuQw8lGarAk=',
+    physicalCharacteristics: 'Bengal, 1 year old, male',
+    behavioralCharacteristics: 'Playful, curious, energetic',
+    notes: 'Needs stimulation and play',
   },
   {
     id: 5,
-    name: "Pringles",
-    photo: "/karsten-winegeart-oU6KZTXhuvk-unsplash.jpg",
-    caracteristicasFisicas: "Golden retriever, 3 years old, male",
-    caracteristicasComportamentais: "Friendly, active, loves playing fetch",
-    observacoes: "Needs regular exercise and socialization",
-  },
-  {
-    id: 6,
-    name: "Max",
-    photo: "/andrew-s-ouo1hbizWwo-unsplash.jpg",
-    caracteristicasFisicas: "Golden retriever, 3 years old, male",
-    caracteristicasComportamentais: "Friendly, active, loves playing fetch",
-    observacoes: "Needs regular exercise and socialization",
+    name: 'Max',
+    species: 'other',
+    photo:
+      'https://media.istockphoto.com/id/1325997570/pt/foto/bengal-cat-lying-on-sofa-and-smiling.jpg?s=1024x1024&w=is&k=20&c=tWRFOQpOAJXYWk5UzGjY0IXE0vslvP8yUuQw8lGarAk=',
+    physicalCharacteristics: 'Bengal, 1 year old, male',
+    behavioralCharacteristics: 'Playful, curious, energetic',
+    notes: 'Needs stimulation and play',
   },
   // Add more pet objects as needed
 ];
