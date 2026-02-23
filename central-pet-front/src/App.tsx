34e9b82e8393e440e16ea841c18af5bad26fcba2
edvardsanta <eduardosantamarco123@outlook.com>
2026-02-23 17:44:03 -0300

    Refactor App component layout and integrate Header, Footer, and SidePanel

--- central-pet-front/src/App.tsx
+++ central-pet-front/src/App.tsx
@@ -1,50 +1,45 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AuthProvider } from "./Contexts/AuthProvider";
-import "./App.css";
-import Layout from "./Layout/Layout";
-import MainPage from "@/Pages/MainPage";
-import RegisterPage from "@/Pages/RegisterPage/RegisterPage";
-import LoginPage from "@/Pages/LoginPage/LoginPage";
-import NotFoundPage from "@/Pages/NotFoundPage/NotFoundPage";
-import AboutPage from "@/Pages/Help/AboutPage/AboutPage";
-import CadastroPetAjuda from "@/Pages/Help/Instructions/CadastroPetAjuda";
-import RegisterPetPage from "@/Pages/Pets/Register/RegisterPet";
-//import PrivateRoute from "./Components/PrivateRoute";
-import "@/styles/global.css";
-import { useEffect } from "react";
-import api from "./services/api";
+import Footer from './Layout/Footer';
+import Header from './Layout/Header';
+import SidePanel from './Components/SidePanel';
+import MainPage from './Pages/MainPage';
+import petsData from '@/Models/Pet.tsx';
 
+// TODO: Revisitar todos os css/tailwind e organizar melhor, tem muita coisa repetida e desnecessária talvez :)
 const App: React.FC = () => {
-  useEffect(() => {
-    const watch = async () => {
-      const teste = await api.get(
-        `${import.meta.env.VITE_REACT_APP_API_BASE_URL}/pets/hello`
-      );
-      console.log(teste);
-    };
-
-    watch();
-  }, []);
-
+  // TODO: Acho que seria melhor calcular esses counts no backend e passar como props para o frontend
+  const dogCount = petsData.filter((pet) => pet.species === 'dog').length;
+  const catCount = petsData.filter((pet) => pet.species === 'cat').length;
+  const otherCount = petsData.filter(
+    (pet) => pet.species !== 'dog' && pet.species !== 'cat',
+  ).length;
   return (
-    <Router>
-      <AuthProvider>
-        <Layout>
-          <Routes>
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/instructions" element={<CadastroPetAjuda />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/" element={<MainPage />} />
-            <Route path="/pets/register" element={<RegisterPetPage />} />
-          </Routes>
-        </Layout>
-      </AuthProvider>
-    </Router>
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+
+      {/* Layout principal */}
+      <div
+        className="
+              pt-[11vh]
+              grid
+              grid-cols-1
+              xl:grid-cols-[1fr_15%]
+            "
+      >
+        {/* Conteúdo */}
+        <main className="px-8 pb-12 overflow-hidden">
+          <MainPage />
+        </main>
+
+        {/* Sidebar */}
+        <aside className="hidden xl:block">
+          {/* SidePanel dinâmico */}
+          <SidePanel dogCount={dogCount} catCount={catCount} otherCount={otherCount} />
+        </aside>
+      </div>
+
+      <Footer />
+    </div>
   );
 };
 
 export default App;
-
-// <Route path="/" element={<MainPage />} />
