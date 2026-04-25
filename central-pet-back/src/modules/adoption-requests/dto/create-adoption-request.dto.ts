a0fa8ab789076da2e744a05e93b5cba46f029cea
GabrielH-Full <gabrielhb2020@gmail.com>
2026-04-25 15:08:06 -0300

    atualizações feita

--- central-pet-back/src/modules/adoption-requests/dto/create-adoption-request.dto.ts
+++ central-pet-back/src/modules/adoption-requests/dto/create-adoption-request.dto.ts
@@ -3,10 +3,6 @@ import { IsIn, IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
 export class CreateAdoptionRequestDto {
   @IsUUID()
   petId: string;
-
-  @IsUUID()
-  requesterId: string;
-
   @IsOptional()
   @IsString()
   @MaxLength(1000)
